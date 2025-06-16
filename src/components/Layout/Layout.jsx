756c13ce4f2803d356f331dcd2d7a9e2589cf9b8
Saso505 <osamasalma921@gmail.com>
2025-06-17 01:34:47 +0300

    first commit

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -4,7 +4,7 @@ import React from "react";
 import style from "./Layout.module.css";
 import Navbar from "../Navbar/Navbar";
 import { Outlet } from "react-router-dom";
-import Footer from "../footer/Footer";
+import Footer from "../Footer/Footer";
 
 export default function Layout() {
   return (
